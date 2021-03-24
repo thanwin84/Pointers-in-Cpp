54b51ab3d7c9cc4d57d0d20372e84695f25e6750
thanwin84 <60491531+thanwin84@users.noreply.github.com>
2021-03-24 19:16:56 +0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 100;
+    int *p = &a;
+    cout << a << endl;
+    cout << "address of a in memory: " << & a<< endl; // adress of a in memory
+    cout <<"address of p in memory: "<< & p << endl; // address of p in memory
+    cout << "address of a pointed by p : " << p << endl; // address of a pointed by p
+    cout << "value at address  pointed by p: "<<*p << endl;
+    * p = 200; // dereferencing
+    cout << "after changing a: " << a << endl;
+    cout << *p << endl;
+    int b = 20;
+    *p =  b; // only value is modified and p still points to a
+    cout << *p << endl;
+    cout << p << endl;
+    cout << a << endl;
+    cout << *p << endl;
+
+    return 0;
+}
